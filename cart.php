f6836de1f973e77a742d2faaf70d9fbbd79cfb75
Yasithan <yasithan.donz@gmail.com>
2020-05-09 11:28:36 +0530

    third commit

--- cart.php
+++ cart.php
@@ -1,3 +1,27 @@
+<?php
+
+    session_start();
+
+    require_once ("php/CreateDB.php");
+    require_once ("php/component.php");
+
+    $db = new CreateDB("Productdb", "Producttb");
+
+
+    if (isset($_POST['remove'])){
+        if ($_GET['action']=='remove'){
+            foreach ($_SESSION['cart'] as $key => $value){
+                if($value["product_id"]==$_GET['id']){
+                    unset($_SESSION['cart']['$key']);
+                    echo"<script>alert('Product has been Removed...!')</script>";
+                    echo"<scrpit>window.location ='cart.php'</scrpit>";
+                }
+            }
+        }
+    }
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -20,6 +44,71 @@
 <?php
     require_once ('php/header.php');
 ?>
+
+<div class="container-fluid">
+    <div class="row px-5">
+        <div class="col-md-7">
+            <div class="shopping-cart">
+                <h6>My Cart</h6>
+                <hr>
+
+                <?php
+                $total = 0;
+                    if (isset($_SESSION['cart'])){
+                        $product_id = array_column($_SESSION['cart'], 'product_id');
+
+                        $result = $db->getData();
+                        while ($row = mysqli_fetch_assoc($result)){
+                            foreach ($product_id as $id){
+                                if ($row['id'] == $id){
+                                    cartElement($row['product_image'], $row['product_name'], $row['product_price'], $row['id']);
+                                    $total=$total + (int)$row['product_price'];
+                                }
+                            }
+                        }
+                    }else{
+                        echo "<h5>Cart is Empty</h5>";
+                    }
+
+                ?>
+
+            </div>
+        </div>
+        <div class="col-md-4 offset-md-1 border rounded mt-5 bg-white h-25">
+
+            <div class="pt-4">
+                <h6>PRICE DETAILS</h6>
+                <hr>
+                <div class="row price-details">
+                    <div class="col-md-6">
+                        <?php
+                            if (isset($_SESSION['cart'])){
+                                $count = count($_SESSION['cart']);
+                                echo "<h6>Price($count items)</h6>";
+                            }else{
+                                echo"<h6>Price(0 items)</h6>";
+                            }
+                        ?>
+                        <h6>Delivery Charges</h6>
+                        <hr>
+                        <h6>Amount Payable</h6>
+                    </div>
+                    <div class="col-md-6">
+                        <h6>$<?php echo $total; ?></h6>
+                        <h6 class="text-success">FREE</h6>
+                        <hr>
+                        <h6>$<?php
+                            echo $total;
+                            ?></h6>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
